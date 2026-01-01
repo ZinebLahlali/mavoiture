c3a7daba4ae3960501aa384c570db56a27677e26
Zineb <zeynepturna1992@gmail.com>
2026-01-01 17:52:45 +0100

    Create a Client and Admin class extends from User class as parent for them with getters and setters

---
+++ classes/Client.php
@@ -0,0 +1,56 @@
+<?php
+require_once "user.php"
+
+class Client extends User 
+{  protected $mobile;
+   protected $adresse;
+   protected $ville
+
+   public function __construct($id_user, $nom, $prenom, $email,$password, $mobile, $adresse, $ville)
+   {  parent::__construct($id_user, $nom, $prenom, $email,$password);
+      $this->mobile = $mobile;
+      $this->adresse = $adresse;
+      $this->ville = $ville;
+   }
+
+   //getters toujour return a //value// 
+   public function getMobile()
+   {
+        return $this->mobile;
+   }
+
+   public function getAdresse()
+   {
+     return $this->adresse;
+   }
+
+   public function getVille()
+   {
+     return $this->ville;
+   }
+
+   //setter pour mettre à jour
+
+   public function setMobile($mobile)
+   {
+      $this->mobile = $mobile;
+   }
+   public function setAdresse($adresse)
+   {
+      $this->adresse = $adresse;
+   }
+
+   public function setVille($ville)
+   {
+      $this->ville = $ville;
+   }
+
+
+
+
+}
+
+
+
+
+?>
